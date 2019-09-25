9c62ee361ca2c100bc84de272b691ee36c79d245
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-09-24 22:14:10 -0400

    Can add to shelf

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
 
   handleClick = (book) => {
     this.setState({
-      shelf: [...this.state.shelf, book]
+      shelf: [book, ...this.state.shelf]
     })
   }
 
--- src/containers/Bookshelf.js
+++ src/containers/Bookshelf.js
@@ -8,8 +8,8 @@ class Bookshelf extends React.Component {
     // console.log("bookshelf props", this.props.book)
     // console.log("bookshelf props 2", this)
 
-    let books = this.props.book.map(bookObj => <Book key = {bookObj.title} shelfBook = {bookObj}/>)
-    console.log(books)
+    let books = this.props.book.map(bookObj => <Book key = {bookObj.id} book = {bookObj}/>)
+    console.log(books) 
     return (
       <div>
         <h1>Book Shelf</h1>
